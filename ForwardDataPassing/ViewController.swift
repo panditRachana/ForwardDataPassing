6178aa7963e7c7f49226b644b1d4fc45d8cb0468
panditRachana <rachupandit93@gmail.com>
2022-11-02 15:06:15 +0530

    Initial Commit

---
+++ ForwardDataPassing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ForwardDataPassing
+//
+//  Created by Rachana Pandit on 02/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
